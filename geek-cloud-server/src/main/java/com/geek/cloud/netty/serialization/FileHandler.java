010d3c1bc02bacd919b69e82128621a389ebb63a
ZhilenkovAndrey <dron19812013@gmail.com>
2022-04-28 11:41:25 +0300

    Hw 3 1.1

---
+++ geek-cloud-server/src/main/java/com/geek/cloud/netty/serialization/FileHandler.java
@@ -0,0 +1,31 @@
+package com.geek.cloud.netty.serialization;
+
+import com.geek.cloud.model.AbstractMessage;
+import com.geek.cloud.model.FileMessage;
+import com.geek.cloud.model.ListMessage;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.extern.slf4j.Slf4j;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Slf4j
+public class FileHandler extends SimpleChannelInboundHandler<AbstractMessage> {
+
+    private final Path serverDir = Path.of("serverFiles");
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ListMessage message = new ListMessage(serverDir);
+        ctx.writeAndFlush(message);
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, AbstractMessage msg) throws Exception {
+        log.info("received: {} message", msg.getMessageType().getName());
+        if (msg instanceof FileMessage file) {
+            Files.write(serverDir.resolve(file.getName()), file.getBytes());
+            ctx.writeAndFlush(new ListMessage(serverDir));
+        }
+    }
+}
