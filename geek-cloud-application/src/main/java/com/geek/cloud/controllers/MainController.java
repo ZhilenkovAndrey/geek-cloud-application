d89d461bfdbec17e85f4a11bba5b8a125418c4b0
ZhilenkovAndrey <dron19812013@gmail.com>
2022-05-05 17:55:39 +0300

    #7 code review.

--- geek-cloud-application/src/main/java/com/geek/cloud/controllers/MainController.java
+++ geek-cloud-application/src/main/java/com/geek/cloud/controllers/MainController.java
@@ -12,6 +12,7 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.ResourceBundle;
 
+
 public class MainController implements Initializable {
     private Path clientDir;
     private Path serverDir = Path.of("serverFiles");
